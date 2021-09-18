cf29c56caf2bf732bfbab836cb7d238e8f0523e8
LachinAshirova <60476768+Lashirova@users.noreply.github.com>
2021-09-18 02:00:22 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+function printId(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+console.log("Lachin");
